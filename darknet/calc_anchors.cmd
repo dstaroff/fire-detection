32599e0dcb04182083279e0e772fa542e992205f
Dmitry Starov <dmitry.staroff@gmail.com>
2021-05-10 14:21:15 +0300

    [ADD]: Train model

---
+++ darknet/calc_anchors.cmd
@@ -0,0 +1,3 @@
+darknet.exe detector calc_anchors data/fire.data -num_of_clusters 9 -width 608 -height 608
+
+pause
---
+++ darknet/train.cmd
@@ -0,0 +1,3 @@
+darknet.exe detector train data/fire.data cfg/tinyyolov3_data_fire.cfg backup/darknet53.conv.74 -map >> log.txt
+
+pause
